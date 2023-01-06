268865eddf009ab2df1c0ae8f8846f3c7d05c728
Roman Ivanov <roman.ivanov.7@gmail.com>
2023-01-06 08:58:35 +0100

    iterator added

--- adt/LinkedListStack.py
+++ adt/LinkedListStack.py
@@ -5,6 +5,8 @@ class Stack:
     def __init__(self):
         self.first = None
         self.n = 0
+        self._cur_node = None
+        self._cur_index = 0
 
     def push(self, item: object):
         old_first = self.first
@@ -26,6 +28,25 @@ class Stack:
     def size(self):
         return self.n
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._cur_node is None:
+            if self.n != 0 and self._cur_index == 0:
+                self._cur_node = self.first
+                node = self._cur_node
+                self._cur_node = self._cur_node.next
+                self._cur_index += 1
+            else:
+                self._cur_index = 0
+                raise StopIteration
+        else:
+            node = self._cur_node
+            self._cur_node = self._cur_node.next
+            self._cur_index += 1
+        return node
+
 
 def test_answer():
     stack = Stack()
@@ -36,3 +57,14 @@ def test_answer():
     assert stack.pop() is 2
     assert stack.pop() is 1
     assert stack.pop() is None
+
+
+def test_iterator():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    a = []
+    for i in stack:
+        a.append(i.item)
+    assert a == [3, 2, 1]
