3f37ac2b300585fca47344cb2bc02571c137df8a
Roman Ivanov <roman.ivanov.7@gmail.com>
2023-02-14 09:34:01 +0100

    bst floor

--- searching/BinarySearchTree.py
+++ searching/BinarySearchTree.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class BinarySearchTree:
     class Node:
         def __init__(self, key: object, val: object):
@@ -45,13 +48,30 @@ class BinarySearchTree:
         node.size = self.size_of_node(node.left) + self.size_of_node(node.right) + 1
         return node
 
+    def floor(self, key: object) -> object:
+        node = self.__find_floor(self.root, key)
+        if node is None:
+            raise Exception("key not found", key)
+        return node.key
 
-def test_put():
+    def __find_floor(self, node: Node, key: object) -> Optional[Node]:
+        if node is None:
+            return None
+        if node.key == key:
+            return node
+        if node.key > key:
+            return self.__find_floor(node.left, key)
+        x = self.__find_floor(node.right, key)
+        return x if x is not None else node
+
+
+def test_op():
     bst = BinarySearchTree()
     bst.put("h", 1)
     bst.put("b", 2)
-    bst.put("c", 3)
+    bst.put("e", 3)
     bst.put("y", 5)
+
     assert bst.root.size is 4
     assert bst.root.key is "h"
     assert bst.root.val is 1
@@ -59,3 +79,7 @@ def test_put():
     assert bst.root.right.val is 5
     assert bst.root.left.key is "b"
     assert bst.root.left.val is 2
+
+    assert bst.floor("d") == "b"
+    assert bst.floor("z") == "y"
+    assert bst.floor("x") == "h"
